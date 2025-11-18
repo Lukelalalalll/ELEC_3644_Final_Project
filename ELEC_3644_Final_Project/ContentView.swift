33aaae5e12d4d0112bcc55a14fa173973ae08c28
Zhang Tianhao <128391830+Lukelalalalll@users.noreply.github.com>
2025-11-18 16:07:03 +0800

    Initial Commit

---
+++ ELEC_3644_Final_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ELEC_3644_Final_Project
+//
+//  Created by cccakkke on 2025/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ELEC_3644_Final_Project/ELEC_3644_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELEC_3644_Final_ProjectApp.swift
+//  ELEC_3644_Final_Project
+//
+//  Created by cccakkke on 2025/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ELEC_3644_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
