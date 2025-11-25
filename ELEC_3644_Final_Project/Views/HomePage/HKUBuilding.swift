2adb23643fb1f85314e15c980c14c209d666ca26
Zhang Tianhao <128391830+Lukelalalalll@users.noreply.github.com>
2025-11-26 03:40:32 +0800

    Complete map function with high precision campus location and convenient navigation to apple map

---
+++ ELEC_3644_Final_Project/Views/HomePage/HKUBuilding.swift
@@ -0,0 +1,113 @@
+//
+//  HKUBuilding.swift
+//  ELEC_3644_Final_Project
+//
+//  Created by cccakkke on 2025/11/26.
+//
+import Foundation
+import CoreLocation
+import SwiftUI
+
+struct HKUBuilding: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+    let description: String
+    let iconName: String
+    let color: Color
+    
+    // 香港大学主要建筑坐标
+    static let allBuildings: [HKUBuilding] = [
+        HKUBuilding(
+            name: "Main Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.2846, longitude: 114.13783),
+            description: "HKU Main Building, historical architecture",
+            iconName: "building.columns",
+            color: .red
+        ),
+        HKUBuilding(
+            name: "Main Library",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28343, longitude: 114.13778),
+            description: "LE Lecture Rooms,Main Library",
+            iconName: "books.vertical",
+            color: .blue
+        ),
+        HKUBuilding(
+            name: "Student Union",
+            coordinate: CLLocationCoordinate2D(latitude: 22.35639, longitude: 114.25195),
+            description: "SU Restaurant",
+            iconName: "person.3",
+            color: .green
+        ),
+        HKUBuilding(
+            name: "MWT Meng Wah Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.2822, longitude: 114.13918),
+            description: "MWT Lecture Rooms",
+            iconName: "sportscourt",
+            color: .orange
+        ),
+        HKUBuilding(
+            name: "CYCC 庄月明文化中心",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28268, longitude: 114.13906),
+            description: "CYM Cultural Centre",
+            iconName: "atom",
+            color: .purple
+        ),
+        HKUBuilding(
+            name: "HW Haking Wong Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28296, longitude: 114.13645),
+            description: "Faculty of Engineering,Haking Wong Building",
+            iconName: "gear",
+            color: .brown
+        ),
+        HKUBuilding(
+            name: "Chong Yuet Ming Chemistry Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28307, longitude: 114.13979),
+            description: "CYCP Lecture Rooms",
+            iconName: "testtube.2",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "Chong Yuet Ming Physics Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28328, longitude: 114.13979),
+            description: "CYPP Lecture Rooms",
+            iconName: "atom",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "Knowles Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28323, longitude: 114.13843),
+            description: "KB Lecture Rooms",
+            iconName: "building.2",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "K.K. Leung Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28327, longitude: 114.13902),
+            description: "KB Lecture Rooms",
+            iconName: "graduationcap",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "Chow Yei Ching Building",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28308, longitude: 114.13544),
+            description: "CB Lecture Rooms",
+            iconName: "briefcase",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "Run Run Shaw Tower",
+            coordinate: CLLocationCoordinate2D(latitude: 22.28369, longitude: 114.13436),
+            description: "RRS/CRT Rooms",
+            iconName: "briefcase",
+            color: .pink
+        ),
+        HKUBuilding(
+            name: "HKU CPD 百週年",
+            coordinate: CLLocationCoordinate2D(latitude: 22.283266, longitude: 114.133957),
+            description: "CPD Rooms",
+            iconName: "briefcase",
+            color: .pink
+        )
+    ]
+}
--- ELEC_3644_Final_Project/Views/HomePage/LocationManager.swift
+++ ELEC_3644_Final_Project/Views/HomePage/LocationManager.swift
@@ -1,47 +1,59 @@
+// LocationManager.swift
+import Foundation
 import CoreLocation
-import Observation
+import MapKit
 
-@Observable
-final class LocationManager: NSObject, CLLocationManagerDelegate {
-    private let manager = CLLocationManager()
-    var currentLocation: CLLocation?
-    var authorizationStatus: CLAuthorizationStatus = .notDetermined
-
-    override init() {
-        super.init()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestWhenInUseAuthorization()
+class LocationManager: NSObject, ObservableObject {
+    static let shared = LocationManager() // 修复：改为 static let
+    
+    private let locationManager = CLLocationManager()
+    
+    @Published var currentLocation: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    var locationHash: Int {
+        return currentLocation?.hashValue ?? 0
     }
-
+    
+    override init() { // 修复：移除 private，改为普通的 init
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    }
+    
     func startUpdatingLocation() {
-        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
-            manager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func requestLocationPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        DispatchQueue.main.async {
+            self.currentLocation = location
         }
     }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        currentLocation = locations.last
-        print("Location Update: \(currentLocation?.coordinate.latitude ?? 0), \(currentLocation?.coordinate.longitude ?? 0)")
-    }
-
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
-        print("AuthorizationStatus: \(authorizationStatus.rawValue)")
-        
-        switch authorizationStatus {
-        case .authorizedWhenInUse, .authorizedAlways:
-            manager.startUpdatingLocation()
-        case .denied, .restricted:
-            print("Location Authorization Refused")
-        case .notDetermined:
-            manager.requestWhenInUseAuthorization()
-        @unknown default:
-            break
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = status
+            if status == .authorizedWhenInUse || status == .authorizedAlways {
+                self.locationManager.startUpdatingLocation()
+            }
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Location Update Failed: \(error.localizedDescription)")
+        print("Location manager error: \(error.localizedDescription)")
     }
 }
